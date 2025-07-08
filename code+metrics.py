a2cbd53cdf3a76a62b056d372baf9f3782490b15
cryptic-13 <f20241150@goa.bits-pilani.ac.in>
2025-07-08 17:07:08 +0530

    Update code+metrics.py

--- code+metrics.py
+++ code+metrics.py
@@ -1,14 +1,15 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras 
+# from tensorflow import keras # This line is no longer needed as we'll use tf.keras directly
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import seaborn as sns # For a nicer confusion matrix plot
 
 # Importing the database required to train the machine learning model:
 def database_import():
-    from keras.datasets import mnist #this will load the database into the model
-    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    # Use tf.keras.datasets to ensure compatibility with the TensorFlow installation
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
     # Reshape and flatten the images uploaded:
     train_images = train_images.reshape((60000, 28*28))
@@ -66,110 +67,97 @@ def initial_parameters(layer_dims) :
     return parameters
 
 
-    #define sigmoid function, which takes the input from previous layer
-    #  --> ΣW(i)A(i) +B (for i ranges from 1 to total number of neurons in the previous layer)
-    # W(i) is the corresponding element (matched by 'i' value) in the weight matrix and
-    # A(i) is the activation level of the ith neuron in the previous layer
-    # to find the activation level of a neuron in the current layer, sigmoid takes this summation and squishes it to a value bw 0 and 1
-
-
+# Define sigmoid function for hidden layers
 def sigmoid(z):
     A= 1/(1 +(np.exp(-z)))
     cache= z
     return (A,cache)
 
-    #cache is returned to facilitate future backpropagation
-    #In forward propagation, the first layer takes its input from the database, processes its output using parameters and input
-    #output is passed to next layer and so on
+# Define softmax function for the output layer (multi-class classification)
+def softmax(z):
+    # Subtract max for numerical stability to prevent overflow with large exponents
+    exp_z = np.exp(z - np.max(z, axis=0, keepdims=True))
+    A = exp_z / np.sum(exp_z, axis=0, keepdims=True)
+    cache = z
+    return A, cache
 
 
 def forward_prop(input, parameters) :
     A= input
     #assign the input to activation level of first layer of neurons
     caches= []
-    neuronCount= len(parameters)//2
-    #because for each neuron, 2 parameters exist, weight and bias
+    neuronCount= len(parameters)//2 # Total number of layers with weights/biases
+
     for x in range (1, neuronCount+1) :
         A_prev= A
-        #store the initial input to first layer neuron
+        # Linear hypothesis: Z = W * A_prev + b
         z= np.dot( parameters['W'+str(x)] ,A_prev ) + parameters['b'+str(x)]
-        # z represents the activation of each neuron in the first layer, after squishification using sigmoid fn
-        # this is the output of the first layer, using the linear hypothesis formula
 
         linear_cache= (A_prev, parameters['W' + str(x)], parameters['b' + str(x)])
 
-
-        A, active_cache= sigmoid(z)
+        # Apply activation function: Softmax for the last layer, Sigmoid for hidden layers
+        if x == neuronCount: # This is the last layer
+            A, active_cache = softmax(z)
+        else: # This is a hidden layer
+            A, active_cache = sigmoid(z)
 
         cache= (linear_cache, active_cache)
         caches.append(cache)
 
-        #linear_cache contains all the individual components that calculated the expression to which sigmoid was appplied
-        #linear_cache stores the activation levels, weights and biases as 3 separate entities
-        #activation cache store the linear hypothesis post matrix multiplication -->  ΣW(i)A(i) +B --> this value
     return A, caches
 
 
 def cost_fn(predicted, truth) :
-    A= predicted
-    #a matrix of all the final output (predictions) made by the neural network
-    #truth is a vector/matrix of all the actual values or true values taken from the training dataset
-    #size= number of data points in training dataset
-
     size= truth.shape[1]
-    # Use element-wise multiplication instead of dot product
-    # Added a small epsilon to log arguments to avoid log(0) which results in -inf
-    epsilon = 1e-10
-    cost= (-1/size) * np.sum(np.multiply(truth, np.log(predicted + epsilon)) + np.multiply(1-truth, np.log(1-predicted + epsilon)))
+    # Categorical Cross-Entropy Loss
+    # Clip predicted values to avoid log(0) or log(1) issues, which can lead to NaN/inf
+    predicted = np.clip(predicted, 1e-10, 1 - 1e-10)
+    cost= (-1/size) * np.sum(truth * np.log(predicted))
     return cost
 
 
 def chain_rule_logic(dA,cache):
     linear_cache, active_cache= cache
     z= active_cache
-    s = 1 / (1 + np.exp(-z)) # Recalculate sigmoid for the derivative
-    dZ=  dA*s * (1-s)
-    #derivative of sigmoid function wrt lienar output of that neuron
+    s = 1 / (1 + np.exp(-z)) # Recalculate sigmoid for its derivative
+    dZ=  dA*s * (1-s) # Derivative of sigmoid function wrt linear output of that neuron
 
     A_prev, W,b= linear_cache
-    count= A_prev.shape[1]
-    #count = number of neurons in previous layer
+    count= A_prev.shape[1] # Number of samples in the batch
+    
     dW= (1/count) * (np.dot(dZ, A_prev.T))
     db= (1/count) * np.sum(dZ, axis=1, keepdims=True)
     dA_prev= np.dot (W.T, dZ)
 
-    #dw, db, dA are the derivatives of the cost function wrt weights, biases and previous activations.
-
+    # dw, db, dA_prev are the derivatives of the cost function wrt weights, biases and previous activations.
     return (dA_prev, dW, db)
 
 
 def backprop (A_last, truth, caches) :
-    #A_last is a vector of activations of the last layer of neurons, ergo the networks final predictions
-    # truth is the vector of actual outputs, or true labels
+    # A_last is a vector of activations of the last layer of neurons (network's final predictions)
+    # truth is the vector of actual outputs, or true labels (one-hot encoded)
 
     gradients = {}
-    layerCount= len (caches)
-    final_neuron_count= A_last.shape[1]
-    truth = truth.reshape(A_last.shape)
+    layerCount= len (caches) # Total number of layers with weights/biases
 
-    # Added a small epsilon to A_last and 1-A_last to avoid division by zero
-    epsilon = 1e-10
-    dAL= -(np.divide(truth, A_last + epsilon) - np.divide(1-truth, 1-A_last + epsilon))
-    current_cache=  caches[layerCount-1]
-    L= layerCount
+    # Derivative of Categorical Cross-Entropy with Softmax for the last layer
+    # This simplifies directly to (A_last - truth)
+    dAL = A_last - truth
 
-    #create a dictionary of all the gradients calculated for backpropagation
-    # each gradient is a partial derivative of the cost fn wrt each parameter
-    # each layer will have a different set of A1, W1, b1 --> parameters and activation
-    # this you can get from its 'caches'
-    # so, for each layer, 3 gradients are computed and stored as a tuple.
-    # this process is repeated for as many layers as there are
+    # Backpropagate for the last layer (L)
+    current_cache_L =  caches[layerCount-1] # Cache for the last layer
+    A_prev_L, W_L, b_L = current_cache_L[0] # linear_cache for the last layer
+    count_L = A_prev_L.shape[1] # Number of samples
 
-    gradients['dA'+str(L-1)], gradients['dW'+ str(L)], gradients['db'+str(L)] = chain_rule_logic(dAL, current_cache)
+    gradients['dW'+ str(layerCount)] = (1/count_L) * (np.dot(dAL, A_prev_L.T))
+    gradients['db'+str(layerCount)] = (1/count_L) * np.sum(dAL, axis=1, keepdims=True)
+    gradients['dA'+str(layerCount-1)] = np.dot (W_L.T, dAL)
 
-    for l in reversed(range(L-1)):
-        current_cache= caches[l]
-        dA_prev_temp, dW_temp, db_temp= chain_rule_logic(gradients['dA'+str(l+1)], current_cache)
+    # Backpropagate through hidden layers (from L-1 down to 1)
+    for l in reversed(range(layerCount-1)): # Iterate from second to last layer down to first hidden layer (index 0)
+        current_cache = caches[l] # Cache for the current layer (l+1 in terms of W/b index)
+        # dA_prev_temp here is the dA for the current layer's activation (which is the input to the next layer's backprop)
+        dA_prev_temp, dW_temp, db_temp = chain_rule_logic(gradients['dA'+str(l+1)], current_cache)
         gradients["dA" + str(l)] = dA_prev_temp
         gradients["dW" + str(l + 1)] = dW_temp
         gradients["db" + str(l + 1)] = db_temp
@@ -179,11 +167,11 @@ def backprop (A_last, truth, caches) :
 
 def update_parameters (parameters, gradients, learning_rate) :
 
-    L = len(parameters) // 2
+    L = len(parameters) // 2 # Number of layers with learnable parameters
 
     for l in range(L):
-        parameters['W'+str(l+1)] = parameters['W'+str(l+1)] -learning_rate*gradients['dW'+str(l+1)]
-        parameters['b'+str(l+1)] = parameters['b'+str(l+1)] -  learning_rate*gradients['db'+str(l+1)]
+        parameters['W'+str(l+1)] = parameters['W'+str(l+1)] - learning_rate * gradients['dW'+str(l+1)]
+        parameters['b'+str(l+1)] = parameters['b'+str(l+1)] - learning_rate * gradients['db'+str(l+1)]
 
     return parameters
 
@@ -256,20 +244,13 @@ def evaluate_model(parameters, X_test, Y_test):
     plt.show()
 
 
-# Assuming you have imported Keras and TensorFlow if running in Colab
-# !pip install tensorflow # Already installed in Colab
-# !pip install keras # Already installed in Colab
-# import tensorflow as tf # Already imported
-# from tensorflow import keras # Already imported
-
-
 # Load the MNIST data
 train_images, train_labels, test_images, test_labels = database_import()
 
 # We define the number of neurons in each layer
 # layer_dims will be defined with the same number of arguments as the number of layers we want, using the shape of train_images
 # and shape of train_labels as our beginning and ending layers
-layer_dims = [train_images.shape[0], 128, train_labels.shape[0]]
+layer_dims = [train_images.shape[0], 128, train_labels.shape[0]] # Input, Hidden, Output
 
 # Set hyperparameters
 epochs = 1000
@@ -320,3 +301,4 @@ print(f"Model Prediction for Random Image: {predicted_digit}")
 # You can compare the predicted_digit with the true label (after decoding the one-hot encoding)
 true_digit = np.argmax(true_label)
 print(f"True Digit for Random Image: {true_digit}")
+
